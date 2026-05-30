3ec79dadbf3e22e3a5c9e5bb3b17546c674bc081
MBHubRoblox <kuncicocokipasanginmeme@gmail.com>
2026-05-30 21:48:50 +0800

    Update scripts.lua

--- scripts.lua
+++ scripts.lua
@@ -12,7 +12,7 @@ local Config = {
 	Secret          = "Code", -- This makes the script ONLY run from the key script. Even if they copy the original obfuscated script to bypass the key, they won't be able to!
 
 	-- [3] Scripts & Links
-	MainScriptURL   = "https://raw.githubusercontent.com/MBHubRoblox/bng/refs/heads/main/for.lua", -- The raw URL of your main script
+	MainScriptURL   = "https://raw.githubusercontent.com/MBHubRoblox/bng/refs/heads/main/luar.lua", -- The raw URL of your main script
 
 	-- [4] Social Media Settings (Set to true to show, false to hide)
 	ShowDiscord     = false,
